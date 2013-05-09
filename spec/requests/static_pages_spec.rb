70f58e631376a8dd26eee83a579b4d77d24bafd6
Louis Tigges <louistigges@outlook.com>
2013-05-10 01:16:01 +0200

    page about toegevoegd

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,23 +6,23 @@ describe "Static pages" do
 
     it "should have the content 'Sample App' " do
 
-    visit '/static_pages/home'
-    page.should have_content ('Sample Add')
+       visit '/static_pages/home'
+       page.should have_content ('Sample App')
     end
   end
 
   describe "Help page" do
     
-      it "should have the content 'Help'" do
-              visit '/static pages/help'
+      it "should have the content 'Help' " do
+              visit '/static_pages/help'
               page.should have_content('Help')
     end
   end
 
   describe "About page" do
-      it "should have the content 'About Us'" do
-      visit '/static pages/about'
-      page.should have content('About Us')
+      it "should have the content 'About Us' " do
+         visit '/static_pages/about'
+         page.should have_content('About Us')
     end
   end
 end
