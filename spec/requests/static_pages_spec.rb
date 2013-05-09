083ea3d3624dbcaeff32244cd5d986acaa2cb102
Louis Tigges <louistigges@outlook.com>
2013-05-10 00:11:36 +0200

    2 pagina's Home en Help toegevoegd

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App' " do
+
+    visit '/static_pages/home'
+    page.should have_content ('Sample Add')
+    end
+  end
+
+  describe "Help page" do
+    
+      it "should have the content 'Help'" do
+              visit '/static pages/help'
+              page.should have_content('Help')
+    end
+  end
+
+  describe "About page" do
+      it "should have the content 'About Us'" do
+      visit '/static pages/about'
+      page.should have content('About Us')
+    end
+  end
+end
